31410cd795b2b214e61759d1d75a829c6d8c2e25
Sandra <sandra0924@protonmail.com>
2024-10-15 15:13:49 +0200

    added content.php

---
+++ content.php
@@ -0,0 +1,13 @@
+<?php
+
+$exercises = [
+    'circle' => [
+        'title' => '4-7-8',
+        'info' => 'Andas in 4, håll andan 7, andas ut 8.<br />
+                   <small>Börjar om 5 sekunder.</small>',
+    ],
+    'square' => [
+        'title' => 'Andas i fyrkant',
+        'info' => 'Andas in… håll andan… andas ut… håll andan…',
+    ],
+];
